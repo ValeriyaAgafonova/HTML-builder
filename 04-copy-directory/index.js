2e3e188601529f66c176f27dd5e2c5de52a6ac2a
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2022-05-23 10:24:18 +0300

    feat: add decision to 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+ 
+
+fs.mkdir('./04-copy-directory/files-copy', { recursive: true}, (err) => {
+  if (err) throw err;
+});
+
+const folder = path.join(__dirname, 'files');
+const copyFolder = path.join(__dirname, 'files-copy');
+
+fs.readdir(folder, (err, files) => {
+  files.forEach(file => {
+    fs.copyFile(`${folder}/${file}`, `${copyFolder}/${file}`, (err) => {  
+      if (err) throw err;
+    });
+  });
+});
+
+fs.readdir(copyFolder, (err, copies) =>{
+  copies.forEach(copy =>{
+    const copyPathInFolder = path.join(folder, copy);
+    const copyPathInCopy = path.join(copyFolder, copy);
+    fs.stat(copyPathInFolder, function(err) {
+      if (err) {
+        console.log('Файл не найден');
+        fs.unlink(copyPathInCopy, function(err){
+          if(err) return console.log(err);
+          console.log('file deleted successfully');
+        });  
+      } 
+    });
+  });
+});
 
