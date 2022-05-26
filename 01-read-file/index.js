25df1b3ef1fe34212ce0954d693e9c7186b83664
ValeriyaAgafonova <86612128+ValeriyaAgafonova@users.noreply.github.com>
2022-05-26 12:58:14 +0300

    fix: change function to avoid return null

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const text = path.join(__dirname, 'text.txt');
 var stream = new fs.ReadStream(text, 'utf-8');
  
-stream.on('readable', function(){
-  var data = stream.read();
+stream.on('data', function(data){
   console.log(data);
 });
